c201b7336d66e327e2f28d190185a9782b6f0f77
PC <bathong448@gmail.com>
2024-06-10 17:04:43 +0700

    updapte

--- src/main/java/com/example/api/Config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/api/Config/JwtAuthenticationFilter.java
@@ -1,6 +1,5 @@
 package com.example.api.Config;
 
-
 import com.example.api.Service.Serviceimpl.JwtService;
 import com.example.api.Service.Serviceimpl.UserServiceimp;
 import jakarta.servlet.FilterChain;
@@ -32,42 +31,39 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
-            @NonNull    HttpServletResponse response,
-            @NonNull    FilterChain filterChain) throws ServletException, IOException {
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
         // lay token tu request
         final String AuthHeader = request.getHeader("Authorization");
         String jwt;
-        //kiem tra xem co ton tai token trong Authorization
-        if(StringUtils.isEmpty(AuthHeader)||!StringUtils.startsWithIgnoreCase(AuthHeader,"Bearer"))
-        {
-          filterChain.doFilter(request,response);
-          return;
+        // kiem tra xem co ton tai token trong Authorization
+        if (StringUtils.isEmpty(AuthHeader) || !StringUtils.startsWithIgnoreCase(AuthHeader, "Bearer")) {
+            filterChain.doFilter(request, response);
+            return;
         }
-        //cat bo Bearer ra khoi chuoi token
+        // cat bo Bearer ra khoi chuoi token
         jwt = AuthHeader.substring(7);
         String username = jwtService.extractUsernamefromToken(jwt);
-        if(!jwt.isEmpty()||jwtService.validateJwtToken(jwt)){
-          //lay thong tin userdetail
-         UserDetails userDetails = userServiceimp.loadUserByUsername(username);
-            //dai dien thong tin xac thuc nguoi dung
-            UsernamePasswordAuthenticationToken authenticationToken= new UsernamePasswordAuthenticationToken(
+        if (!jwt.isEmpty() || jwtService.validateJwtToken(jwt)) {
+            // lay thong tin userdetail
+            UserDetails userDetails = userServiceimp.loadUserByUsername(username);
+            // dai dien thong tin xac thuc nguoi dung
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
                     userDetails,
                     null,
-                    userDetails.getAuthorities()
-            );
-            if(authenticationToken.isAuthenticated() ){
-                //  được sử dụng để thiết lập chi tiết về quá trình xác thực của người dùng
+                    userDetails.getAuthorities());
+            if (authenticationToken.isAuthenticated()) {
+                // được sử dụng để thiết lập chi tiết về quá trình xác thực của người dùng
                 // , bao gồm thông tin về yêu cầu HTTP mà yêu cầu xác thực được thực hiện.
                 authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                //luu thong tin da xac thuc  vao  SecurityContextHolder
+                // luu thong tin da xac thuc vao SecurityContextHolder
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken);
 
-            }else {
-                throw  new InvalidClassException("thong tin that bai");
+            } else {
+                throw new InvalidClassException("thong tin that bai");
             }
 
-            filterChain.doFilter(request,response);
-
+            filterChain.doFilter(request, response);
 
         }
 
